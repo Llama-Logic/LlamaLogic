edef331539343d579f225cd0103baf827c7a1079
Daniel Henry <daniel.henry@epiforge.com>
2024-07-20 21:52:11 -0400

    global usings; CLS compliance

--- Common/AssemblyInfo.cs
+++ Common/AssemblyInfo.cs
@@ -1,6 +1,2 @@
-using System;
-using System.Runtime.InteropServices;
-
-[assembly: CLSCompliant(true)]
 [assembly: ComVisible(false)]
 [assembly: DefaultDllImportSearchPaths(DllImportSearchPath.SafeDirectories)]
--- LlamaLogic.Packages/Usings.cs
+++ LlamaLogic.Packages/Usings.cs
@@ -1,7 +1,12 @@
+global using ICSharpCode.SharpZipLib.Zip.Compression.Streams;
 global using System;
+global using System.Buffers;
+global using System.Collections.Generic;
 global using System.Diagnostics.CodeAnalysis;
 global using System.Globalization;
 global using System.IO;
+global using System.Linq;
+global using System.Runtime.CompilerServices;
 global using System.Runtime.InteropServices;
 global using System.Text;
 global using System.Text.RegularExpressions;
