1d955cbd4502977b19d3e5e15af91d9e01f92182
Daniel Henry <daniel.henry@epiforge.com>
2024-09-28 23:27:59 -0400

    Packages: added properties to the manifest format allowing dependencies to be ignored by agents based on the presence/absence or other mods and packs

--- LlamaLogic.Packages/Models/ModFileManifest/ModFileManifestModelRequiredMod.cs
+++ LlamaLogic.Packages/Models/ModFileManifest/ModFileManifestModelRequiredMod.cs
@@ -19,6 +19,30 @@ public sealed class ModFileManifestModelRequiredMod :
     public HashSet<ImmutableArray<byte>> Hashes { get; private set; } = [];
 
     /// <summary>
+    /// Instructs agents to ignore fulfilling this dependency if this hash is present in the player's catalog of mods
+    /// </summary>
+    [YamlMember(Order = 9, DefaultValuesHandling = DefaultValuesHandling.OmitDefaults)]
+    public ImmutableArray<byte> IgnoreIfHashAvailable { get; set; }
+
+    /// <summary>
+    /// Instructs agents to ignore fulfilling this dependency if this hash is not present in the player's catalog of mods
+    /// </summary>
+    [YamlMember(Order = 10, DefaultValuesHandling = DefaultValuesHandling.OmitDefaults)]
+    public ImmutableArray<byte> IgnoreIfHashUnavailable { get; set; }
+
+    /// <summary>
+    /// Instructs agents to ignore fulfilling this dependency if the pack identified by this pack code is present (e.g. "EP01" for Get to Work)
+    /// </summary>
+    [YamlMember(Order = 11, DefaultValuesHandling = DefaultValuesHandling.OmitNull)]
+    public string? IgnoreIfPackAvailable { get; set; }
+
+    /// <summary>
+    /// Instructs agents to ignore fulfilling this dependency if the pack identified by this pack code is not present (e.g. "EP01" for Get to Work)
+    /// </summary>
+    [YamlMember(Order = 12, DefaultValuesHandling = DefaultValuesHandling.OmitNull)]
+    public string? IgnoreIfPackUnavailable { get; set; }
+
+    /// <summary>
     /// Gets/sets the <see cref="ResourceKey"/> of the mod manifest <see cref="ResourceType.SnippetTuning"/> of the dependency mod (optional)
     /// </summary>
     [YamlMember(Order = 5, DefaultValuesHandling = DefaultValuesHandling.OmitNull)]
@@ -81,7 +105,15 @@ public sealed class ModFileManifestModelRequiredMod :
             else
             {
                 var tunableValue = reader.ReadElementContentAsString();
-                if (tunableName == "mod_manifest_key")
+                if (tunableName == "ignore_if_hash_available")
+                    IgnoreIfHashAvailable = tunableValue.ToByteSequence().ToImmutableArray();
+                else if (tunableName == "ignore_if_hash_unavailable")
+                    IgnoreIfHashUnavailable = tunableValue.ToByteSequence().ToImmutableArray();
+                else if (tunableName == "ignore_if_pack_available")
+                    IgnoreIfPackAvailable = tunableValue;
+                else if (tunableName == "ignore_if_pash_unavailable")
+                    IgnoreIfPackUnavailable = tunableValue;
+                else if (tunableName == "mod_manifest_key")
                     ModManifestKey = ResourceKey.Parse(tunableValue);
                 else if (tunableName == "name")
                     Name = tunableValue;
@@ -107,6 +139,10 @@ public sealed class ModFileManifestModelRequiredMod :
         writer.WriteTunableList("required_features", RequiredFeatures);
         writer.WriteTunable("requirement_identifier", RequirementIdentifier);
         writer.WriteTunableList("hashes", Hashes);
+        writer.WriteTunable("ignore_if_hash_available", IgnoreIfHashAvailable);
+        writer.WriteTunable("ignore_if_hash_unavailable", IgnoreIfHashUnavailable);
+        writer.WriteTunable("ignore_if_pack_available", IgnoreIfPackAvailable);
+        writer.WriteTunable("ignore_if_pash_unavailable", IgnoreIfPackUnavailable);
         writer.WriteEndElement();
     }
 
